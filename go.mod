520319abe296e2e9784b61289f9887300ef2b55d
Xuan Nam Tran <nam2311.dtd@gmail.com>
2025-06-29 11:24:57 +0700

    Using tcell to display information

--- go.mod
+++ go.mod
@@ -4,4 +4,16 @@ go 1.22.2
 
 require github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203
 
-require golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+require (
+	github.com/gdamore/encoding v1.0.1 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/rivo/uniseg v0.4.3 // indirect
+	golang.org/x/term v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+)
+
+require (
+	github.com/gdamore/tcell/v2 v2.8.1
+	golang.org/x/sys v0.29.0 // indirect
+)
--- main.go
+++ main.go
@@ -6,66 +6,132 @@ import (
 	fff "fuzzy-file-finder/src"
 	"fuzzy-file-finder/src/algos"
 	"fuzzy-file-finder/src/fileTree"
-	"sync"
-	"unicode/utf8"
-
 	"github.com/eiannone/keyboard"
+	"github.com/gdamore/tcell/v2"
+	"sync"
+	"time"
+	"unicode/utf8"
 )
 
 var wg sync.WaitGroup
+var screen tcell.Screen
+var queryStyle tcell.Style
+var resultsStyle tcell.Style
 
 func main() {
-
-	ctx, cancel := context.WithCancel(context.Background())
-	jobs := make(chan string, 400000)
-	query := ""
-	fs := fileTree.CreateDirTreeStruct()
-	root := "C:\\"
-
+	//Keyboard setup
 	err := keyboard.Open()
 	if err != nil {
 		panic(err)
 	}
 	defer keyboard.Close()
 
-	fileTree.CreateAsyncJob(root, jobs, fs, 10)
+	//Screen setup
+	screen, err = tcell.NewScreen()
+	if err != nil {
+		panic(err)
+	}
+	if err := screen.Init(); err != nil {
+		panic(err)
+	}
+	w, _ := screen.Size()
+	defer screen.Fini()
+	clearScreen()
+	queryStyle = queryStyle.Background(tcell.Color111).Foreground(tcell.ColorBlack)
+	initTerminalStyling(w)
 
+	//Matcher and scorer setup
+	ctx, cancel := context.WithCancel(context.Background())
+	jobs := make(chan string, 400000)
+	lastQueryLen := 0
+	query := ""
+	fs := fileTree.CreateDirTreeStruct()
+	root := "C:\\"
+	ticker := time.NewTicker(300 * time.Millisecond)
+
+	fileTree.CreateAsyncJob(root, jobs, fs, 10)
+	drawText(1, 0, fmt.Sprintf("Query: %s", query), queryStyle)
+	screen.Show()
+
+	// Poll for updates from results
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				if len(query) == 0 || len(query) == lastQueryLen {
+					continue
+				}
+				cancel()
+				resultsChan := make(chan algos.MatchResult, 10000)
+				ctx, cancel = context.WithCancel(context.Background())
+				algos.CreateNewWorker(ctx, fs, resultsChan, 4, query, &wg)
+				go func() {
+					defer close(resultsChan)
+					wg.Wait()
+				}()
+				scoreResults := fff.NewResultArray(20)
+				for res := range resultsChan {
+					scoreResults.Add(res)
+				}
+				y := 1
+				for _, res := range scoreResults.Holder {
+					drawText(1, y, fmt.Sprintf("[%d] %s", res.Score, res.Candidate), resultsStyle)
+					y++
+				}
+				lastQueryLen = len(query)
+				screen.Show()
+			}
+		}
+	}()
+
+	// Reading user inputs key-by-key
 	for {
 		char, key, err := keyboard.GetKey()
 		if err != nil {
 			panic(err)
 		}
 		if key == keyboard.KeyEsc {
+			ticker.Stop()
 			break
 		}
 		if key == keyboard.KeyBackspace || key == keyboard.KeyBackspace2 {
 			if len(query) > 0 {
 				_, size := utf8.DecodeLastRuneInString(query)
 				query = query[:len(query)-size]
+				lastQueryLen = len(query) + 1
 			}
 		} else if char >= 32 && char <= 126 {
 			query += string(char)
-			cancel()
-			resultsChan := make(chan algos.MatchResult, 10000)
-			clearScreen()
-			fmt.Printf("Query: %s\n", query)
-			ctx, cancel = context.WithCancel(context.Background())
-			algos.CreateNewWorker(ctx, fs, resultsChan, 4, query, &wg)
-			go func() {
-				defer close(resultsChan)
-				wg.Wait()
-			}()
-			scoreResults := fff.NewResultArray(20)
-			for res := range resultsChan {
-				scoreResults.Add(res)
-			}
-			for _, result := range scoreResults.Holder {
-				fmt.Printf("[%d] %s \n", result.Score, result.Candidate)
-			}
+			lastQueryLen = len(query) - 1
 		}
+		clearRow(0, w, queryStyle)
+		drawText(1, 0, fmt.Sprintf("Query: %s", query), queryStyle)
+		screen.Show()
 	}
 }
 
 func clearScreen() {
-	fmt.Print("\033[2J\033[H")
+	screen.Clear()
+}
+
+func drawText(x, y int, text string, style tcell.Style) {
+	for i, ch := range text {
+		screen.SetContent(x+i, y, ch, nil, style)
+	}
+}
+
+func clearRow(y int, width int, style tcell.Style) {
+	for x := range width {
+		screen.SetContent(x, y, ' ', nil, style)
+	}
+}
+
+func setRowStyle(y int, width int, style tcell.Style) {
+	for x := range width {
+		screen.SetContent(x, y, ' ', nil, style)
+	}
+}
+
+func initTerminalStyling(w int) {
+	setRowStyle(0, w, queryStyle)
 }
